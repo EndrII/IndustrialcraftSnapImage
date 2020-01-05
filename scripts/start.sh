f94b305097bc3a260b337ea3eedf815e7cfba19d
Andrei <EndrIIMail@gmail.com>
2020-01-05 20:23:40 +0300

    fix create packages

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
-java -Xms128m -Xmx3500m -jar forge-1.12.2-14.23.5.2768-universal.jar nogui
+FORGE_SERVER=$HOME/forge-1.12.2-14.23.5.2768-universal.jar
+
+if [ -f $FORGE_SERVER ]; then
+    cp -r $SNAP/serverImage/* $HOME/
+fi
+
+java -Xms128m -Xmx3500m -jar "$FORGE_SERVER" nogui
